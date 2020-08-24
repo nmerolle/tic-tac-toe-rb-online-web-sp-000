65acf29d2c4c60f7767de5fd4eacbf8dd0c8a3fd
Nicholas Merolle <nmerolle11@hotmail.com>
2020-08-24 13:41:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -110,7 +110,7 @@ def play(board)
     turn(board)
   end
   if won?(board)
-    return "Congratulations #{winner(board)}!"
+    puts "Congratulations #{winner(board)}!"
     elsif draw?(board)
     puts "Cat's Game!"
   end
